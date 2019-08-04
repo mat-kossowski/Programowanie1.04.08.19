81786cec5c089362a33f4d74218bbbb094ffa73c
mat-kossowski <mat.kossowski.pl@gmail.com>
2019-08-04 10:00:47 +0200

    some change

--- src/main/java/pl/sdaacademy/Application.java
+++ src/main/java/pl/sdaacademy/Application.java
@@ -3,5 +3,8 @@ package pl.sdaacademy;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Siemka Ziomeczku !!! ");
+
+
     }
 }
