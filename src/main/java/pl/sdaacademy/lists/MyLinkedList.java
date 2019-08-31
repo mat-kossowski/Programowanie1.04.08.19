152e0403763c238047c2225d66f82e4e0d319a84
mat-kossowski <53392895+mat-kossowski@users.noreply.github.com>
2019-08-31 12:04:27 +0200

    Linked list (#3)
    * linked list
    
    * linked list, methods (get)
    
    * linked list, methods (remove)
    
    * linked list methods add on index

---
+++ src/main/java/pl/sdaacademy/lists/MyLinkedList.java
@@ -0,0 +1,96 @@
+package pl.sdaacademy.lists;
+
+public class MyLinkedList {
+
+    private MyLinkedListItem head;
+    private int size;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void add(Integer value) {
+        MyLinkedListItem myLinkedListItem = new MyLinkedListItem(value);
+
+        if (this.head == null) {
+            this.head = myLinkedListItem;
+        } else {
+
+            MyLinkedListItem lastItem = getLastItem();
+            lastItem.nextItem = myLinkedListItem;
+        }
+        size++;
+    }
+
+
+
+
+    public Integer get(int index) {
+
+        return getElement(index).valeu;
+    }
+
+
+    private MyLinkedListItem getElement(int index) {
+        MyLinkedListItem tmpItem = head;
+        for (int i = 0; i < index; i++) {
+            tmpItem = tmpItem.nextItem;
+        }
+        return tmpItem;
+    }
+
+
+    public void remove(int index) {
+
+        MyLinkedListItem previous = getElement(index - 1);
+        MyLinkedListItem toRemove = getElement(index);
+        previous.nextItem = toRemove.nextItem;
+        toRemove.nextItem=null;
+
+        size--;
+
+
+//        /\
+//       /  \
+//        ||
+//        ||
+//        ||
+    }
+
+    public void add (int index, Integer value){
+        MyLinkedListItem toAdd = new MyLinkedListItem(value);
+        MyLinkedListItem previousElement = getElement(index - 1);
+        toAdd.nextItem = previousElement.nextItem;
+        previousElement.nextItem = toAdd;
+
+        size++;
+    }
+
+
+
+
+    public int size() {
+        return size;
+    }
+
+    private MyLinkedListItem getLastItem() {
+        MyLinkedListItem tmpItem = this.head;
+        while (tmpItem.nextItem != null) {
+            tmpItem = tmpItem.nextItem;
+        }
+        return tmpItem;
+    }
+
+
+    static class MyLinkedListItem {
+        private Integer valeu;
+        private MyLinkedListItem nextItem;
+
+        private MyLinkedListItem(Integer valeu) {
+            this.valeu = valeu;
+        }
+    }
+
+
+}
---
+++ src/test/java/pl/sdaacademy/lists/MyLinkedListTest.java
@@ -0,0 +1,83 @@
+package pl.sdaacademy.lists;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyLinkedListTest {
+
+
+    @Test
+    public void addShouldAddElementToEmptyList(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        //then
+        myLinkedList.add(5);
+        //when
+        Assert.assertEquals(1,myLinkedList.size());
+    }
+    @Test
+    public void addShouldAddElementToNonEmptyList(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        //then
+        myLinkedList.add(5);
+        //when
+        Assert.assertEquals(5,myLinkedList.size());
+    }
+@Test
+public void getShouldReturnFirstElement(){
+        //TODO - dopisac testy
+}
+
+@Test
+public void getShouldReturnLastElement(){
+        //TODO - dopisac testy
+}
+
+    @Test
+    public void getShouldReturnMiddleElement(){
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+        myLinkedList.add(20);
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+        myLinkedList.add(50);
+
+        //when
+
+        Integer result = myLinkedList.get(3);
+        //then
+
+        Assert.assertEquals((Integer)30, result);
+    }
+
+    @Test
+    public void removeShouldRemoveMiddleElement() {
+        //given
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+        myLinkedList.add(20);
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+
+        //then
+        myLinkedList.remove(2);
+
+        //when
+
+        Assert.assertEquals(4,myLinkedList.size());
+        Assert.assertEquals((Integer) 0,myLinkedList.get(0));
+        Assert.assertEquals((Integer) 10,myLinkedList.get(1));
+        Assert.assertEquals((Integer) 30,myLinkedList.get(2));
+        Assert.assertEquals((Integer) 40,myLinkedList.get(3));
+    }
+}
